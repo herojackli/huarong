0e9c6abc62cd48d64414b8198763b23ec0a23195
jack-liangliang <15517153996@163.com>
2022-08-23 20:39:53 +0800

    创建模板

---
+++ pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+    <view class="content">
+        <image class="logo" src="/static/logo.png"></image>
+        <view class="text-area">
+            <text class="title">{{title}}</text>
+        </view>
+        <view style="padding: 20px;">
+            <u-button type="primary" text="确定"></u-button>
+            <u-button type="primary" :plain="true" text="镂空"></u-button>
+            <u-button type="primary" :plain="true" :hairline="true" text="细边"></u-button>
+            <u-button type="primary" loading loadingText="加载中"></u-button>
+            <u-button type="primary" icon="map" text="图标按钮"></u-button>
+            <u-button type="primary" shape="circle" text="按钮形状"></u-button>
+            <u-button text="渐变色按钮" color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"></u-button>
+            <u-button type="primary" size="small" text="大小尺寸"></u-button>
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: 'Hello'
+            }
+        },
+        onLoad() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .logo {
+        height: 200rpx;
+        width: 200rpx;
+        margin-top: 200rpx;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 50rpx;
+    }
+
+    .text-area {
+        display: flex;
+        justify-content: center;
+    }
+
+    .title {
+        font-size: 36rpx;
+        color: #8f8f94;
+    }
+</style>
